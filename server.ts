acd2b6b95211efd286c2d28777f024e9c2dedcc3
Henrik Panhans <henrik@panhans.dev>
2021-11-03 23:07:26 +0100

    Removing secure cookies

--- server.ts
+++ server.ts
@@ -19,10 +19,6 @@ var sess = {
   cookie: { secure: false },
 }
 
-if (app.get('env') === 'production') {
-  sess.cookie.secure = true // serve secure cookies
-}
-
 app.use(session(sess))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
