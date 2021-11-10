468c73d74a36c6f8af16aab25b39328a4f59a7f3
Henrik Panhans <henrik@panhans.dev>
2021-11-10 15:11:43 +0100

    Including SCSS map

--- app/services/sass.service.ts
+++ app/services/sass.service.ts
@@ -6,9 +6,14 @@ export async function renderSassAndWriteToPublicDirectory() {
   const renderPromise = promisify(sass.render)
   const result = await renderPromise({
     file: `${process.cwd()}/app/styles/common.scss`,
-    outFile: `${process.cwd()}/app/public/styles/common.css`
+    outFile: `${process.cwd()}/app/public/styles/common.css`,
+    sourceMap: true,
+    sourceMapContents: true,
+    outputStyle: 'compressed',
+    sourceMapRoot: '/styles/common'
   })
 
   const writeFilePromise = promisify(writeFile)
   await writeFilePromise(`${process.cwd()}/app/public/styles/common.css`, result.css, 'utf8')
+  await writeFilePromise(`${process.cwd()}/app/public/styles/common.css.map`, result.map, 'utf8')
 }
