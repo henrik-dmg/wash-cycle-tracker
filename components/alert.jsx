f22cbf2ccd995576e675e0a1f2a280ca2f965b8e
Henrik Panhans <henrik@panhans.dev>
2022-07-12 21:28:05 +0200

    Clean up
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- components/alert.jsx
+++ components/alert.jsx
@@ -2,14 +2,13 @@ import styles from './alert.module.css';
 import classnames from 'classnames';
 
 export default function Alert({ children, type }) {
-  return (
-    <div
-      className={classnames({
+  const classname = classnames({
         [styles.success]: type === 'success',
         [styles.error]: type === 'error',
         [styles.warning]: type === 'warning',
-      })}
-    >
+  })
+  return (
+    <div className={classname}>
       {children}
     </div>
   );
