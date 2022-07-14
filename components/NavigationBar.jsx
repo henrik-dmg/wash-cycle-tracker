fdf348e6705a746ff3225d9b8840900447f379fe
Henrik Panhans <henrik@panhans.dev>
2022-07-14 16:26:49 +0200

    Add NavigationBar
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

---
+++ components/NavigationBar.jsx
@@ -0,0 +1,47 @@
+import { useUser } from '@auth0/nextjs-auth0'
+import Image from 'next/image'
+import Link from 'next/link'
+import Loader from './Loader'
+
+export default function NavigationBar() {
+  const { user, isLoading } = useUser()
+
+  function getUserID() {
+    return user.sub.split('|')[1]
+  }
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <a className="btn-logo">FEED</a>
+          </Link>
+        </li>
+
+        {!user && (
+          <li className="push-left">
+            <a href="/api/auth/login">Login</a>
+          </li>
+        )}
+        {user && (
+          <>
+            <li className="push-left">
+              <Link href={`/${getUserID()}`}>
+                <Image src={user.picture} alt={user.name} width={100} height={100} />
+              </Link>
+            </li>
+            <li>
+              <a href="/api/auth/logout">Logout</a>
+            </li>
+          </>
+        )}
+
+        {isLoading && (
+          <li>
+            <Loader isShown />
+          </li>
+        )}
+      </ul>
+    </nav>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import React from 'react'
 import { UserProvider } from '@auth0/nextjs-auth0'
+import NavigationBar from  '../components/NavigationBar'
 
 function WashingMachineApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
+      <NavigationBar />
       <Component {...pageProps} />
     </UserProvider>
   )
